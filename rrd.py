a47345d51bda4522e72ab2897da11caa0e1058d4
Nils Schneider <nils@nilsschneider.net>
2013-11-23 21:54:52 +0100

    rrd.py: clientCount -= 1 might not be needed with gluon/alfred

--- rrd.py
+++ rrd.py
@@ -118,7 +118,7 @@ class rrd:
         if not node.flags['client']:
           nodes[node.id] = node
           node.clients = 0;
-          clientCount -= 1
+          clientCount -= 1 # XXX: might not be needed with gluon/alfred
         else:
           clientCount += 1
     for link in db.get_links():
