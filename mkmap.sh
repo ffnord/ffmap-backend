37db42b51ec8c1628a27674709083b591677f57a
Nils Schneider <nils@nilsschneider.net>
2014-02-07 11:03:27 +0100

    mkmap.sh: enable alfred

--- mkmap.sh
+++ mkmap.sh
@@ -12,4 +12,4 @@ cd "$(dirname "$0")"/
 ./ffhlwiki.py http://freifunk.metameute.de/wiki/Knoten > aliases_hl.json
 ./ffhlwiki.py http://freifunk.metameute.de/wiki/Moelln:Knoten > aliases_moelln.json
 
-./bat2nodes.py -a aliases.json -a aliases_hl.json -a aliases_moelln.json -d $DEST
+./bat2nodes.py -A -a aliases.json -a aliases_hl.json -a aliases_moelln.json -d $DEST
