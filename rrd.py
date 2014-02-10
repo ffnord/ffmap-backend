17fcf10b74fbda384e26ecb480b77c46a1a3dbc5
Jan-Philipp Litza <janphilipp@litza.de>
2014-02-10 21:45:32 +0100

    Offload rrdtool calls and model to separate module

--- rrd.py
+++ rrd.py
@@ -2,6 +2,7 @@
 import subprocess
 import time
 import os
+from RRD import RRD, DS, RRA
 
 class rrd:
   def __init__( self
@@ -11,7 +12,7 @@ class rrd:
               , displayTimeNode = "1d"
               ):
     self.dbPath = databaseDirectory
-    self.globalDbFile = databaseDirectory + "/nodes.rrd"
+    self.globalDb = RRD(databaseDirectory + "/nodes.rrd")
     self.imagePath = imagePath
     self.displayTimeGlobal = displayTimeGlobal
     self.displayTimeNode = displayTimeNode
@@ -24,39 +25,29 @@ class rrd:
     except:
       os.mkdir(self.imagePath)
 
-  def checkAndCreateIfNeededGlobalDatabase(self):
-    """ Creates the global database file iff it did not exist.
-    """
-    if not os.path.exists(self.globalDbFile):
-      # Create Database with rrdtool
-      args =  ["rrdtool",'create', self.globalDbFile
-              ,'--start', str(round(self.currentTimeInt - 60))
-              ,'--step' , '60'
-              # Number of nodes available
-              ,'DS:nodes:GAUGE:120:0:U'
-              # Number of client available
-              ,'DS:clients:GAUGE:120:0:U'
-              ,'RRA:AVERAGE:0.5:1:120'
-              ,'RRA:AVERAGE:0.5:60:744'
-              ,'RRA:AVERAGE:0.5:1440:1780'
-              ]
-      subprocess.call(args)
-
   def updateGlobalDatabase(self,nodeCount,clientCount):
     """ Adds a new (#Nodes,#Clients) entry to the global database.
     """
-    # Update Global RRDatabase
-    args =  ["rrdtool",'updatev', self.globalDbFile
-            # #Nodes #Clients
-            , self.currentTime + ":"+str(nodeCount)+":"+str(clientCount)
-            ]
-    subprocess.check_output(args)
+    self.globalDb.ensureSanity(
+      [
+        # Number of nodes available
+        DS('nodes', 'GAUGE', 120, 0, float('NaN')),
+        # Number of client available
+        DS('clients', 'GAUGE', 120, 0, float('NaN')),
+      ], [
+        RRA('LAST', 0, 1, 44640),
+        RRA('LAST', 0, 60, 744),
+        RRA('LAST', 0, 1440, 1780),
+      ],
+      step=60,
+    )
+    self.globalDb.update({'nodes': nodeCount, 'clients': clientCount})
 
   def createGlobalGraph(self):
     nodeGraph = self.imagePath + "/" + "globalGraph.png"
     args = ["rrdtool", 'graph', nodeGraph, '-s', '-' + self.displayTimeGlobal, '-w', '800', '-h' '400'
-           ,'DEF:nodes=' + self.globalDbFile + ':nodes:AVERAGE', 'LINE1:nodes#F00:nodes\\l'
-           ,'DEF:clients=' + self.globalDbFile + ':clients:AVERAGE','LINE2:clients#00F:clients'
+           ,'DEF:nodes=' + self.globalDb.filename + ':nodes:AVERAGE', 'LINE1:nodes#F00:nodes\\l'
+           ,'DEF:clients=' + self.globalDb.filename + ':clients:AVERAGE','LINE2:clients#00F:clients'
            ]
     subprocess.check_output(args)
 
@@ -67,26 +58,20 @@ class rrd:
   def nodeMACToPNGFile(self,nodeMAC):
     return self.imagePath + "/" + str(nodeMAC).replace(":","") + ".png"
 
-  def checkAndCreateIfNeededNodeDatabase(self,nodePrimaryMAC):
-    # TODO check for bad nodeNames
-    nodeFile = self.nodeMACToRRDFile(nodePrimaryMAC);
-    if not os.path.exists(nodeFile):
-      # TODO Skalen anpassen
-      args = ["rrdtool",'create',nodeFile
-             ,'--start',str(round(self.currentTimeInt - 60))
-             ,'--step' , '60'
-             ,'DS:upstate:GAUGE:120:0:1'
-             ,'DS:clients:GAUGE:120:0:200'
-             ,'RRA:AVERAGE:0.5:1:120'
-             ,'RRA:AVERAGE:0.5:5:1440'
-             ,'RRA:AVERAGE:0.5:60:720'
-             ,'RRA:AVERAGE:0.5:720:730'
-             ]
-      subprocess.check_output(args)
-
   # Call only if node is up
   def updateNodeDatabase(self,nodePrimaryMAC,clientCount):
+    # TODO check for bad nodeNames
     nodeFile = self.nodeMACToRRDFile(nodePrimaryMAC)
+    nodeRRD = RRD(nodeFile)
+    nodeRRD.ensureSanity(
+      [
+        DS('upstate', 'GAUGE', 120, 0, 1),
+        DS('clients', 'GAUGE', 120, 0, float('NaN')),
+      ], [
+        RRA('LAST', 0, 1, 44640),
+      ],
+      step=60,
+    )
     # Update Global RRDatabase
     args =  ["rrdtool",'updatev', nodeFile
             # #Upstate #Clients
@@ -128,10 +113,8 @@ class rrd:
       elif target in nodes and not source in nodes:
         nodes[target].clients += 1
 
-    self.checkAndCreateIfNeededGlobalDatabase()
     self.updateGlobalDatabase(len(nodes),clientCount)
     for mac in nodes:
-      self.checkAndCreateIfNeededNodeDatabase(mac)
       self.updateNodeDatabase(mac,nodes[mac].clients)
 
   def update_images(self):
