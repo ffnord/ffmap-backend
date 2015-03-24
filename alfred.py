15a0f71847f307a651c9d0e991e44a402e07e16d
Nils Schneider <nils@nilsschneider.net>
2015-03-24 18:18:40 +0100

    alfred.py: remove unused function aliases

--- alfred.py
+++ alfred.py
@@ -14,32 +14,3 @@ def statistics():
 
 def vis():
   return _fetch(160)
-
-def aliases():
-  alias = {}
-  for node in nodeinfo():
-    node_alias = {}
-    if 'location' in node:
-      try:
-        node_alias['gps'] = str(node['location']['latitude']) + ' ' + str(node['location']['longitude'])
-      except:
-        pass
-
-    try:
-      node_alias['firmware'] = node['software']['firmware']['release']
-    except KeyError:
-      pass
-
-    try:
-      node_alias['id'] = node['network']['mac']
-    except KeyError:
-      pass
-
-    if 'hostname' in node:
-      node_alias['name'] = node['hostname']
-    elif 'name' in node:
-      node_alias['name'] = node['name']
-    if len(node_alias):
-      alias[node['network']['mac']] = node_alias
-
-  return alias
